cb770b7dcd9b2bef21baabc623e214c07ebc720d
Deniz <lunk4ter@gmail.com>
2025-06-17 01:21:06 -0500

    TBS

--- engine.go
+++ engine.go
@@ -7,65 +7,63 @@ import (
 )
 
 type ScoredTrieNode struct {
-	Champion          string
+	championName      string
 	AverageEvaluation int
 	CompletedStates   []string
 	Children          map[string]*ScoredTrieNode
 }
 
+const SupportRole = "Support"
+
 const DataDir = "data/"
 
-var DraftOrder = []string{"T1GB", "T2GB", "T2B", "T1B", "T1P", "T2P", "T2P", "T1P", "T1B", "T2B", "T1P", "T2P"}
+var DraftOrder = []string{"T1GB1", "T2GB1", "T2B2", "T1B2", "T1P1", "T2P1", "T2P2", "T1P2", "T1B3", "T2B3", "T1P3", "T2P3"}
 
 func main() {
 	champions := data.GetChampionsFromCsv(DataDir + data.ChampionsShortListCsvFilename)
-	championNamesSet := make(map[string]bool, len(champions))
+	ChampionToID, IDToChampion := initializeChampionMaps(champions)
+
+	championSet := make(map[string]data.Champion, len(champions))
 	for _, champion := range champions {
-		championNamesSet[champion.Name] = true
-	}
-	ChampionToID, IDToChampion := initializeChampionMaps(championNamesSet)
-
-	vroomVroom(championNamesSet, ChampionToID, IDToChampion)
-}
-
-func initializeChampionMaps(champions map[string]bool) (map[string]string, map[string]string) {
-	ChampionToID := make(map[string]string, len(champions))
-	IDToChampion := make(map[string]string, len(champions))
-
-	i := 0
-	for champion := range champions {
-		var id string
-		if i < 10 {
-			id = fmt.Sprintf("%d", i)
-		} else {
-			id = string(rune('A' + i - 10))
-		}
-
-		ChampionToID[champion] = id
-		IDToChampion[id] = champion
-		i++
+		championSet[champion.ID] = champion
 	}
 
-	return ChampionToID, IDToChampion
+	vroomVroom(championSet, ChampionToID, IDToChampion)
 }
 
-func vroomVroom(champions map[string]bool, ChampionToID map[string]string, IDToChampion map[string]string) {
+func initializeChampionMaps(champions []data.Champion) (map[string]string, map[string]string) {
+	ChampionNameToID := make(map[string]string, len(champions))
+	IDToChampionName := make(map[string]string, len(champions))
+
+	for _, champion := range champions {
+		ChampionNameToID[champion.Name] = champion.ID
+		IDToChampionName[champion.ID] = champion.Name
+	}
+
+	return ChampionNameToID, IDToChampionName
+}
+
+func vroomVroom(champions map[string]data.Champion, ChampionNameToID map[string]string, IDToChampionName map[string]string) {
 	node := ScoredTrieNode{
-		Champion:          "",
+		championName:      "",
 		AverageEvaluation: 0,
 		CompletedStates:   []string{},
 		Children:          make(map[string]*ScoredTrieNode),
 	}
 
 	evaluationSum := 0
-	for champion := range champions {
+	for championId, champion := range champions {
 		// kick off a process for each champion getting first chosen
 		evaluation, completedStates := process(
-			champions,
-			ChampionToID,
-			IDToChampion,
+			len(champions),
+			ChampionNameToID,
+			IDToChampionName,
 			"",
-			champion,
+			championId,
+			0,
+			false,
+			false,
+			0,
 			0,
 			copyMap(champions),
 			copyMap(champions),
@@ -82,36 +80,43 @@ func vroomVroom(champions map[string]bool, ChampionToID map[string]string, IDToC
 }
 
 func process(
-	champions map[string]bool,
-	ChampionToID map[string]string,
-	IDToChampion map[string]string,
+	numChampions int,
+	ChampionNameToID map[string]string,
+	IDToChampionName map[string]string,
 	previousState string,
-	chosenChampion string,
-	currentDraftStep int,
+	chosenChampionId string,
+	currDraftStepIdx int,
+	T1HasSupport bool,
+	T2HasSupport bool,
+	numT1Picks int,
+	numT2Picks int,
 	T1PickableChampions map[string]bool,
 	T1BannableChampions map[string]bool,
 	T2PickableChampions map[string]bool,
 	T2BannableChampions map[string]bool,
 ) (int, []string) {
-	currentState := previousState + ChampionToID[chosenChampion]
+	currentState := previousState + chosenChampionId
 
 	// Base case
-	if currentDraftStep >= len(DraftOrder) {
+	if currDraftStepIdx >= len(DraftOrder) {
 		evaluation := 9
 		// TODO: Evaluate completed state.
-		// TODO: Prune if no support.
 		return evaluation, []string{currentState}
 	}
 
 	node := ScoredTrieNode{
-		Champion:          chosenChampion,
+		championName:      IDToChampionName[chosenChampionId],
 		AverageEvaluation: 0,
 		CompletedStates:   []string{},
 		Children:          make(map[string]*ScoredTrieNode),
 	}
 
 	selectableChampions := getSelectableChampions(
-		currentDraftStep,
+		currDraftStepIdx,
+		T1HasSupport,
+		T2HasSupport,
+		numT1Picks,
+		numT2Picks,
 		T1PickableChampions,
 		T1BannableChampions,
 		T2PickableChampions,
@@ -119,28 +124,35 @@ func process(
 	)
 
 	evaluationSum := 0
-	for champion := range selectableChampions {
+	for championId, champion := range selectableChampions {
+
 		copyT1PickableChampions := copyMap(T1PickableChampions)
 		copyT1BannableChampions := copyMap(T1BannableChampions)
 		copyT2PickableChampions := copyMap(T2PickableChampions)
 		copyT2BannableChampions := copyMap(T2BannableChampions)
 
-		updateChampionSlicesInPlace(
-			currentDraftStep,
+		numT1Picks, numT2Picks = updateSelectableChampionsInPlace(
+			championId,
+			currDraftStepIdx,
+			numT1Picks,
+			numT2Picks,
 			copyT1PickableChampions,
 			copyT1BannableChampions,
 			copyT2PickableChampions,
 			copyT2BannableChampions,
-			champion,
 		)
 
 		evaluation, completedStates := process(
-			champions,
-			ChampionToID,
-			IDToChampion,
+			numChampions,
+			ChampionNameToID,
+			IDToChampionName,
 			currentState,
-			champion,
-			currentDraftStep+1,
+			championId,
+			currDraftStepIdx+1,
+			T1HasSupport || DraftOrder[currDraftStepIdx][1] == '1' || champion.Role == SupportRole,
+			T2HasSupport || DraftOrder[currDraftStepIdx][1] == '2' || champion.Role == SupportRole,
+			numT1Picks,
+			numT2Picks,
 			copyT1PickableChampions,
 			copyT1BannableChampions,
 			copyT2PickableChampions,
@@ -151,18 +163,31 @@ func process(
 		node.CompletedStates = append(node.CompletedStates, completedStates...)
 	}
 
-	node.AverageEvaluation = evaluationSum / len(champions)
+	node.AverageEvaluation = evaluationSum / numChampions
 	return node.AverageEvaluation, node.CompletedStates
 }
 
 func getSelectableChampions(
-	currentDraftStep int,
+	currDraftStepIdx int,
+	T1HasSupport bool,
+	T2HasSupport bool,
+	numT1Picks int,
+	numT2Picks int,
 	T1PickableChampions map[string]bool,
 	T1BannableChampions map[string]bool,
 	T2PickableChampions map[string]bool,
 	T2BannableChampions map[string]bool,
-) map[string]bool {
-	switch DraftOrder[currentDraftStep] {
+) map[string]data.Champion {
+	/*
+	 * TODO: Prune child nodes that don't have at least 1 support in the comp
+	 * if T1 is picking
+	 * and T1 has already made 2 picks
+	 * and T1 has no sups
+	 * return only supports
+	 *
+	 */
+
+	switch DraftOrder[currDraftStepIdx] {
 	case "T1P":
 		return T1PickableChampions
 	case "T1GB", "T1B":
@@ -177,44 +202,47 @@ func getSelectableChampions(
 	}
 }
 
-func updateChampionSlicesInPlace(
-	currentDraftStep int,
+func updateSelectableChampionsInPlace(
+	champion string,
+	currDraftStepIdx int,
+	numT1Picks int,
+	numT2Picks int,
 	T1PickableChampions map[string]bool,
 	T1BannableChampions map[string]bool,
 	T2PickableChampions map[string]bool,
 	T2BannableChampions map[string]bool,
-	champion string,
-) {
-	switch DraftOrder[currentDraftStep] {
+) (int, int) {
+	switch DraftOrder[currDraftStepIdx] {
 	case "T1GB":
 		delete(T1PickableChampions, champion)
 		delete(T1BannableChampions, champion)
 		delete(T2PickableChampions, champion)
 		delete(T2BannableChampions, champion)
-		break
+		return numT1Picks, numT2Picks
 	case "T1B":
 		delete(T1BannableChampions, champion)
 		delete(T2PickableChampions, champion)
-		break
+		return numT1Picks, numT2Picks
 	case "T1P":
 		delete(T1PickableChampions, champion)
 		delete(T2BannableChampions, champion)
-		break
+		return numT1Picks + 1, numT2Picks
 	case "T2GB":
 		delete(T1PickableChampions, champion)
 		delete(T1BannableChampions, champion)
 		delete(T2PickableChampions, champion)
 		delete(T2BannableChampions, champion)
-		break
+		return numT1Picks, numT2Picks
 	case "T2B":
 		delete(T1PickableChampions, champion)
 		delete(T2BannableChampions, champion)
-		break
+		return numT1Picks, numT2Picks
 	case "T2P":
 		delete(T2PickableChampions, champion)
 		delete(T1BannableChampions, champion)
-		break
+		return numT1Picks, numT2Picks + 1
 	default:
 		log.Fatal("Invalid draft step.")
+		return numT1Picks, numT2Picks
 	}
 }
